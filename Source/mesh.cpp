18746c47a05b23c1a6ba46aa45d52b37f04a4289
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-30 13:23:40 -0400

    more mesh stuff

--- Source/mesh.cpp
+++ Source/mesh.cpp
@@ -14,57 +14,29 @@ void setup_geometry(Geometry* geometry) {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, geometry->EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, geometry->indicies.size() * sizeof(unsigned int), &geometry->indicies[0], GL_STATIC_DRAW);
 
-    int attribute_index = 0;
+    int stride = 0;
     for (int i = 0; i < geometry->attributes.size(); i++) {
+        stride += geometry->attributes[i];
+    }
+
+    int attribute_index = 0;
+    int vertex_attribute_offset = 0;
+    for (u32 i = 0; i <  geometry->attributes.size(); i++) {
         unsigned int attribute = geometry->attributes[i];
         
         if (attribute == 0) {
             continue;
         }
 
-        glEnableVertexAttribArray(0);
-	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+        glEnableVertexAttribArray(attribute_index);
+        glVertexAttribPointer(attribute_index, attribute, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(vertex_attribute_offset * sizeof(float)));
+        vertex_attribute_offset += attribute;
 
         attribute_index++;
-    }
+    }    
 
-	// Vertex Positions
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
-
-	// Vertex Normals
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Normal));
-
-	// Vertex Texture Coords
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, TexCoords));
-
-	
-	// COLOR STUFF AT 3 if you want
-
-	/*
-
-	glEnableVertexAttribArray(3);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Color));
-	 
-	// Vertex Tangent
-	glEnableVertexAttribArray(4);
-	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Tangent));
-
-	// Vertex Bitangent
-	glEnableVertexAttribArray(5);
-	glVertexAttribPointer(5, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Bitangent));
-
-	// Bone IDs
-	glEnableVertexAttribArray(6);
-	glVertexAttribIPointer(6, 4, GL_INT, sizeof(Vertex), (void *)offsetof(Vertex, m_BoneIDs));
-
-	// Weights
-	glEnableVertexAttribArray(7);
-	glVertexAttribPointer(7, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, m_Weights));
-	*/
-	glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindVertexArray(0); 
 }
 
 Geometry Geometry::Cube(std::vector<unsigned int> attributes = {3, 0, 3, 2}) {

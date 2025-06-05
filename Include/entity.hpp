2481c7b2ac418072568a653e51f0189c70400973
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-06-05 19:20:33 -0400

    Update entity.hpp

--- Include/entity.hpp
+++ Include/entity.hpp
@@ -1,40 +1,33 @@
-struct Transform {
-    GM_Vec3 position;
-    Quaternion orientation;
-    GM_Vec3 scale;
-};
+#include <gm.h>
+#include <mesh.hpp>
 
 // Date: June 03, 2025
 // TODO(Jovanni): I think the reference ID is a unique ID for serialization?
 // The reference_id is for pointing another entity at this reference in a stable way
 // think of like a puzzle where you reference some type of trigger entity you would use the reference_id
-typedef struct EntityID {
+struct EntityID {
     u64 reference_id;
     u32 entity_index;
-} EntityID;
+};
 
-typedef enum EntityType {
+enum EntityType {
     ENTITY_TYPE_NONE,
     ENTITY_TYPE_PLAYER,
     ENTITY_TYPE_BALL,
     ENTITY_TYPE_BLOCK,
     ENTITY_TYPE_POWERUP,
-} EntityType;
+};
 
-typedef struct Entity {
+struct Entity {
     // base
     EntityID identifer;
     EntityType type;
-    Transform transform;
+
+    GM_Vec3 position;
+    GM_Quaternion orientation;
+    GM_Vec3 scale;
+
     Mesh mesh;
-    Material material;
 
     // gameplay
-    Damage damage;
-    StatusEffect status_effect;
-    Resource resource;
-
-    // puzzles
-    Interactable interactable;
-    Triggerable triggerable;
-} Entity;
+};

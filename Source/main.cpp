8092e8a8898c4f481e565252ec1cc4615db93328
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-06-22 15:45:03 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,6 +12,31 @@ int main() {
     GLFWwindow* window = application.initalizeWindow();
     ckg_assert_msg(window, "failed to initalize glfw or glad\n");
 
+    // Date: June 22, 2025
+    // TODO(Jovanni):
+    /*
+    typedef struct GameState {
+        CKG_Arena* permanent_arena; // General-purpose permanent memory
+        CKG_Arena* entity_arena;    // For entity structs
+        CKG_Arena* asset_arena;     // Textures, models, sounds, etc.
+        CKG_Arena* evictable_arena; // For things that can be evicted
+        CKG_Arena* scratch_arena;   // Frame-local scratch (temporary, reset every tick/frame)
+
+        struct {
+            float gravity;    // defaults to -9.8
+            float time_scale; // defaults to  1.0
+        } rules;
+
+        u64 next_unique_reference_id; // stable and unique
+        Entity* entities[256];
+        // entity flags 
+        // transparent
+        // skybox
+        // flags that will dictate what shaders get used?
+        u32 entity_count;
+    } GameState;
+    */
+
     IOD_GLFW_SETUP(window);
     application.initalizeResources();
     application.initalizeInputBindings();
